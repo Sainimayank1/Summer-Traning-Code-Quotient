29256ab78e6eed0758ac0888e7e215a4c88b42b2
Mayank saini <mayanksaini4455@gmail.com>
2023-07-30 17:22:25 +0530

    Done assignment - 6

--- Assingment-6/server.js
+++ Assingment-6/server.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from "express"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import session from "express-session";
 import fs from 'fs'
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,18 +10,35 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(
+    session({
+        secret: "iamasecret on a windows",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 app.use((req, res, next) => {
     console.log(req.url);
     next()
 })
 
+app.get("/dashboard", (req, res) => {
+    if (req.session.isLoggedIn) {
+        res.status(200).sendFile(__dirname + "/public/dashboard.html");
+    }
+    else
+        res.redirect("/login")
+})
 
 app.get("/login", (req, res) => {
-    res.status(200).sendFile(__dirname + "/public/login.html");
+    if (req.session.isLoggedIn)
+        res.redirect("/dashboard")
+    else
+        res.status(200).sendFile(__dirname + "/public/login.html");
 })
 
 app.post("/login", function (req, res) {
-    const {email, password } = req.body;
+    const { email, password } = req.body;
     try {
         fs.readFile(__dirname + "/userDetail.mp4", 'utf8', function (err, data) {
             if (err)
@@ -29,16 +47,18 @@ app.post("/login", function (req, res) {
             let isFinded = false;
             let userName = "";
             todos.map((val) => {
-                if (val.email === email)
-                {
+                if (val.email === email && val.password === password) {
                     isFinded = true;
                     userName = val.name;
                 }
             })
-            if(isFinded)
-                res.status(200).redirect("/dashboard")
+            if (isFinded) {
+                req.session.isLoggedIn = true;
+                req.session.username = userName;
+                res.redirect("/dashboard")
+            }
             else
-                res.status(400).json({msg:"User already exists"});
+                res.status(400).json({ msg: "User or password are wrong" });
 
 
         });
@@ -46,7 +66,7 @@ app.post("/login", function (req, res) {
         console.log(error)
         res.status(400).json(error)
     }
-    
+
 
 });
 
@@ -66,17 +86,20 @@ app.post("/register", function (req, res) {
                 if (val.email === email)
                     isFinded = true;
             })
-            if(isFinded)
-                 res.status(400).json({msg:"User already exists"});
+
+            if (isFinded) {
+                res.status(400).json({ msg: "User already exists" });
+                return;
+            }
             else
                 todos.push({ "name": name, "email": email, "password": password });
 
 
             fs.writeFile("./userDetail.mp4", JSON.stringify(todos), function (error) {
                 if (error)
-                    res.status(400).json({msg:"Something went wrong in Writefile module"});
+                    res.status(400).json({ msg: "Something went wrong in Writefile module" });
                 else
-                    res.status(200).json({msg:"Succesfully created user"});
+                    res.status(200).json({ msg: "Succesfully created user" });
             });
 
         });
@@ -86,8 +109,14 @@ app.post("/register", function (req, res) {
 
 });
 
-app.get("/dashboard", (req, res) => {
-    res.status(200).sendFile(__dirname + "/public/dashboard.html");
+
+app.get("/logout", (req, res) => {
+    if (req.session.isLoggedIn) {
+        req.session.destroy((err) => {
+            if (!err)
+                res.redirect("/login")
+        })
+    }
 })
 
 
